6d8e6c8ed9d92480bfae0f1f76a0f32cc1bc2f2f
ToobaaZubair <toobaazubair@gmail.com>
2024-01-27 17:13:29 +0500

    Game

---
+++ index.ts
@@ -0,0 +1,33 @@
+
+import { log } from "console";
+import inquirer from "inquirer";
+const systemGeneratedNumber = Math.floor(Math.random() * 10)
+
+const Answers = await inquirer.prompt([
+    {
+        type: `number`,
+        name: `GuessNUmber`,
+        message: `Write Your Guessed Number Between 1 to 10:`
+    }
+])
+
+let Answer: number
+if (Answers.GuessNUmber === systemGeneratedNumber) {
+    console.log(`Yeahhhhhh You Guessed Right One / You Win The Game !`);
+
+}
+else if (Answers.GuessNUmber > systemGeneratedNumber) {
+    console.log(`Too High ! Try a lower Number`);
+
+}
+else if (Answers.GuessNUmber < systemGeneratedNumber) {
+    console.log(`Too Low ! Try a higher Number`);
+
+}
+else if (Answers.GuessNUmber !== systemGeneratedNumber) {
+    console.log(`Try Again Better Luck Next Time`);
+
+}
+console.log(Answers.GuessNUmber, `Guessed Number`);
+console.log(systemGeneratedNumber, `System Generated Number`);
+
